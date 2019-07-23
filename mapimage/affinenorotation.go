63943aca37520c2af0425317109de5a57b779905
Andrew Dodd <andrew.john.dodd@gmail.com>
2019-07-23 22:19:12 +0200

    Add comments to help readers from possible blog post

--- mapimage/affinenorotation.go
+++ mapimage/affinenorotation.go
@@ -9,11 +9,41 @@ type AffineNoRotTransformation struct {
 }
 
 func NewAffineNoRotTransformation(Sx, Sy, Tx, Ty float64) AffineNoRotTransformation {
+	//   --
+	// | Sx |
+	// | Sy |
+	// | Tx |
+	// | Ty |
+	//   --
+
 	trans := mat.NewDense(4, 1, []float64{Sx, Sy, Tx, Ty})
-	//printMat("trans", trans)
 	return AffineNoRotTransformation{trans}
 }
 
+/*
+
+    System 1                             System 2
+
+    ^                                    ^
+    |                                    |
+    |    B---------C                     |
+    |    |         |                     |     b---------------c
+    |    |         |            =        |     |               |
+    |    |         |                     |     |               |
+    |    A---------D                     |     a---------------d
+    |                                    |
+    +-------------------->               +--------------------------->
+
+	Supply matching points from both coordinate systems. I.e:
+
+	proj := NewAffineNoRotTransformationFromPoints([A, C], [a, c])
+	proj.Project(b) => returns B
+
+	 or for the opposite projection:
+
+	proj := NewAffineNoRotTransformationFromPoints([a, c], [A, C])
+	proj.Project(B) => returns b
+*/
 func NewAffineNoRotTransformationFromPoints(standardPoints []Point, localPoints []Point) (AffineNoRotTransformation, error) {
 	n1, e1 := standardPoints[0].Lat, standardPoints[0].Lng
 	n2, e2 := standardPoints[1].Lat, standardPoints[1].Lng
@@ -28,20 +58,29 @@ func NewAffineNoRotTransformationFromPoints(standardPoints []Point, localPoints 
 		0, y2, 0, 1,
 	})
 
+	//          X         *    t    =    e
+	//   --------------       ----      ----
+	//  | x1  0  1  0  |     | Sx |    | e1 |
+	//  |  0 y1  0  1  |  *  | Sy | =  | n1 |
+	//  | x2  0  1  0  |     | Tx |    | e2 |
+	//  |  0 y2  0  1  |     | Ty |    | n2 |
+	//   --------------       ----      ----
+
 	var trans mat.Dense
+	// https://en.wikipedia.org/wiki/System_of_linear_equations
+	// Expressed in the form Ax=b (or Xt=e)
+	// I.e. solve for T
+	// T = X^-1 * E
 	err := trans.Solve(X, E)
 	if err != nil {
 		return AffineNoRotTransformation{}, err
 	}
 
-	// printMat("E", E)
-	// printMat("X", X)
-	// printMat("trans", &trans)
-
 	return AffineNoRotTransformation{&trans}, nil
 }
 
 func (t *AffineNoRotTransformation) Project(p Point) Point {
+	// Project just one point and retrieve it from the returned slice
 	return t.Projects(p)[0]
 }
 
@@ -50,6 +89,20 @@ func (t *AffineNoRotTransformation) Projects(points ...Point) (results []Point) 
 		return
 	}
 
+	// Build "X" such that there are two rows for each input point:
+	//
+	//          X         *    t    =    e
+	//   --------------       ----      ----
+	//  | x1  0  1  0  |     | Sx |    | e1 |
+	//  |  0 y1  0  1  |  *  | Sy | =  | n1 |
+	//  | x2  0  1  0  |     | Tx |    | e2 |
+	//  |  0 y2  0  1  |     | Ty |    | n2 |
+	//  |  .  .  .  .  |      ----|    |  . |
+	//  |  .  .  .  .  |               |  . |
+	//  |  .  .  .  .  |               |  . |
+	//  | xN  0  1  0  |               | eN |
+	//  |  0 yN  0  1  |               | nN |
+	//   --------------                 ----
 	X := mat.NewDense(2*len(points), 4, nil)
 	for i, p := range points {
 		y := p.Lat
@@ -58,7 +111,6 @@ func (t *AffineNoRotTransformation) Projects(points ...Point) (results []Point) 
 		X.SetRow(2*i+1, []float64{0, y, 0, 1})
 	}
 
-	// printMat("trans", t.trans)
 	var transformed mat.Dense
 	transformed.Mul(X, t.trans)
 
